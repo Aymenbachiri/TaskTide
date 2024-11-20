9ef74f6dfc107886d76eed8ced1deb1a1c938f7d
Aymenbachiri <aymen.bachiri99@gmail.com>
2024-11-20 13:55:13 +0100

    update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,7 +9,7 @@ const staticRoutes = [
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const routes = staticRoutes.map((route) => ({
-    url: `https://tasktide.vercel.app${route.path}`,
+    url: `https://tasktidepro.vercel.app${route.path}`,
     lastModified: route.lastModified,
     changeFrequency: "weekly" as const,
     priority: route.path === "" ? 1 : 0.8,
